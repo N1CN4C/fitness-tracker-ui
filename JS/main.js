386f878b5b1e3dc3a492df39f770c19c8ced993e
N1CN4C <npbrauer@gmail.com>
2025-09-16 21:16:30 -0500

    Movements table, add movement button and modal.

--- JS/main.js
+++ JS/main.js
@@ -1,18 +1,43 @@
+// ====== DATA ======
+let movements = [];
 
-document.addEventListener('DOMContentLoaded', function() {
-  const dropdown = document.querySelector('.dropdown');
-  const toggleBtn = dropdown.querySelector('.dropdown_toggle');
+// ====== DOM ELEMENTS ======
 
-  toggleBtn.addEventListener('click', function(e) {
-    const isOpen = dropdown.classList.toggle('open');
-    toggleBtn.setAttribute('aria-expanded', isOpen);
-  });
+// Header Nav
+const dropdown = document.querySelector('.dropdown');
+const toggleBtn = dropdown.querySelector('.dropdown_toggle');
 
-  // Close menu when clicking outside
-  document.addEventListener('click', function(e) {
-    if (!dropdown.contains(e.target)) {
-      dropdown.classList.remove('open');
-      toggleBtn.setAttribute('aria-expanded', 'false');
-    }
-  });
+// Modals
+const modalMovementAdd = document.getElementById('modal-movement-add');
+const btnMovementAdd = document.getElementById('btn-movement-add');
+const btnModalMovementAddClose = modalMovementAdd.querySelector('.close');
+
+// ====== EVENT LISTENERS ======
+
+// Header Nav
+toggleBtn.addEventListener('click', function(e) {
+  const isOpen = dropdown.classList.toggle('open');
+  toggleBtn.setAttribute('aria-expanded', isOpen);
 });
+
+// Close Nav menu when clicking outside
+document.addEventListener('click', function(e) {
+  if (!dropdown.contains(e.target)) {
+    dropdown.classList.remove('open');
+    toggleBtn.setAttribute('aria-expanded', 'false');
+  }
+});
+
+// Modals
+btnMovementAdd.addEventListener('click', openAddMovementModal );
+btnModalMovementAddClose.addEventListener('click', closeAddMovementModal );
+
+// ====== FUNCTIONS ======
+
+// Modals
+function openAddMovementModal() {
+  modalMovementAdd.style.display = 'block';
+}
+function closeAddMovementModal() {
+  modalMovementAdd.style.display = 'none';
+}
